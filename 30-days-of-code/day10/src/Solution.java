6de8038b2c649c4ace4eec1233ae7df2d9f32bde
rocamador <xrocamador@gmail.com>
2020-10-19 21:31:50 +0100

    day 10

---
+++ 30-days-of-code/day10/src/Solution.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static int maxConsecutiveOnes(int n) {
+        String s = Integer.toBinaryString(n);
+        //System.out.printf("%s (%s)\n", s, n);
+        int max = 0;
+        char last;
+        int curr = 0;
+
+        for (char ch: s.toCharArray()) {
+            if (ch == '1')
+                curr++;
+            else {
+                curr=0;
+            }
+            if (max < curr)
+                max = curr;
+        }
+        return max;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        scanner.close();
+        System.out.println(maxConsecutiveOnes(n));
+    }
+}
